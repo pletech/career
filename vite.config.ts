10811da897eb09a45b6ae290ecb07747fe200203
pletech <d.seo@pletech.co.jp>
2026-04-22 18:32:18 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/career-path-src/',
+  base: '/career/',
   plugins: [react(), tailwindcss()],
 })
